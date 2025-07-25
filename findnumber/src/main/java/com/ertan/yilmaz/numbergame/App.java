6bfb9c4ec7fea6cd45e7c1497a726274d0f9e73b
ylmzzertann <ertanylmz2012@hotmail.com>
2025-07-24 23:48:58 -0400

    Initial Commit

---
+++ findnumber/src/main/java/com/ertan/yilmaz/numbergame/App.java
@@ -0,0 +1,13 @@
+package com.ertan.yilmaz.numbergame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
